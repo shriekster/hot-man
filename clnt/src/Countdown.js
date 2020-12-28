3eaa78622568622947d4ffee9acb213c1fa58f9b
shriekster <shrieksterr@gmail.com>
2020-12-28 12:26:40 +0200

    Password reset complete (#?)

--- clnt/src/Countdown.js
+++ clnt/src/Countdown.js
@@ -10,7 +10,12 @@ class Countdown extends React.Component {
     this.timerID = 0;
 
     this.state = {
-      //
+      start: 'fas fa-hourglass-start',
+      half: 'fas fa-hourglass-half',
+      end: 'fas fa-hourglass-end',
+
+      seconds: 'seconds',
+      secondsFill: 'seconds-fill'
     }
   }
 
@@ -33,10 +38,36 @@ class Countdown extends React.Component {
   }
 
   render() {
+    let timeout = this.props.timeout;
+
+    /** Minutes and seconds */
+    let mins = `0${Math.floor(timeout / 60)}`;
+    let secs = timeout % 60 < 10 ? `0${timeout % 60}` : timeout % 60;
+
+    /** Hourglass */
+    let hourglass;
+    let seconds = this.state.seconds;
+    let secondsFill = this.state.secondsFill;
+    
+    if (timeout > 65) {
+      hourglass = this.state.start;
+    } else
+    if (10 < timeout && timeout <= 65) {
+      hourglass = this.state.half;
+    } else {
+      hourglass = this.state.end + ' warned';
+    }
+
+    let percent = Math.fround(((120 - timeout) / 120) * 100);
+
     return (
       <span className='countdown bold'>
-          <span><i className='fas fa-stopwatch stopwatch red'></i></span> 
-          <span className='seconds red'>{this.props.timeout}</span>
+          <span><i className={hourglass +' stopwatch red'}></i></span> 
+          <span className={seconds}>
+            {`${mins}:${secs}`}
+            <span className={secondsFill}
+              style={{width: `${percent}%`}}>{`${mins}:${secs}`}</span>
+          </span>
           
       </span>
     );
