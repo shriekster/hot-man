beb979777d0c55fdc2f8a4f9575683c95e440b63
shriekster <shriekster@gmail.com>
2021-02-03 15:08:19 +0200

    test2

--- clnt/src/CentralizatorSpatii.js
+++ clnt/src/CentralizatorSpatii.js
@@ -4,7 +4,7 @@ import Select from 'react-select';
 
 import Spinner from './Spinner';
 
-
+//////////////////////////////////////////////////////////////////////////////////
 class CentralizatorSpatii extends React.Component {
   constructor(props) {
     super(props);
